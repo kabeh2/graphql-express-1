1f51479a4b962aec6faceb10739879a67f645589
kabeh2 <mattcnk@hotmail.com>
2020-08-26 23:22:39 -0400

    Initial commit + intro schemas with graphql-express

---
+++ schema/schema.js
@@ -0,0 +1,38 @@
+const graphql = require('graphql');
+const axios = require('axios');
+
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLID },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args, ctx, info) {
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((resp) => resp.data);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
